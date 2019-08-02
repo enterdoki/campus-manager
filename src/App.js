399a6639178ad7eea09c3b1ddc88a5c5c73f22fd
Toms <chen.tommy925@gmail.com>
2019-08-02 12:12:36 -0400

    init students

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import AllCampus from './components/allCampus'
+import AllStudents from './components/allStudents'
 
 class App extends Component {
   render(){
     const allCampusComponent = () =>(<AllCampus/>);
+    const allStudentsComponent = () =>(<AllStudents/>)
     return (
       <Router>
           <div>
             <Route exact path="/" render={allCampusComponent}/>
+            <Route exact path="/students" render={allStudentsComponent}/>
           </div>
         </Router>
     );
