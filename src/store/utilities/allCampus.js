0ebb363d3d45a12853d6213fd35ad07fcb66ccd4
Steven Li <ilumii@users.noreply.github.com>
2019-08-05 14:36:16 -0400

    Campus popup

--- src/store/utilities/allCampus.js
+++ src/store/utilities/allCampus.js
@@ -1,5 +1,6 @@
 const FETCH_CAMPUS = "FETCH_CAMPUS";
 const DELETE_CAMPUS = "DELETE_CAMPUS";
+const ADD_CAMPUS = "ADD_CAMPUS"
 
 const fetchAllCampus = (campus) => {
     return{
@@ -16,10 +17,27 @@ const removeCampus = (id) =>{
     }
 }
 
+const addCampus = () =>{
+    const arr = {
+            "campusId": 4,
+            "campus": "Hunter College",
+            "email": "jerryjingle@bells.com",
+            "imageUrl": "http://i.imgur.com/AItCxSs.jpg",
+            "gpa": null,
+            "createdAt": "2018-12-06T19:58:21.314Z",
+            "updatedAt": "2018-12-06T19:58:21.314Z",
+    }
+    return {
+        type: ADD_CAMPUS,
+        payload: arr
+    }
+}
 export const deleteCampusThunk = (id) => (dispatch) =>{
     dispatch(removeCampus(id));
 }
-
+export const addCampusThunk = () => (dispatch) =>{
+    dispatch(addCampus());
+}
 export const fetchAllCampusThunk = () => (dispatch) =>{
     const arrayOfCampusFromApi = [
         {
@@ -77,6 +95,8 @@ export default (state = [], action) =>{
             return action.payload;
         case DELETE_CAMPUS:
             return state.filter(campus=>campus.campusId !== action.payload)
+        case ADD_CAMPUS:
+            return [...state, action.payload];
         default:
             return state;
     }
