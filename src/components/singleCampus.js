d2997c7ae07885ac18b42bd22419934371a61f80
enterdoki <chen.tommy925@gmail.com>
2019-08-11 22:46:22 -0400

    axios get fail

--- src/components/singleCampus.js
+++ src/components/singleCampus.js
@@ -1,41 +1,61 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import axios from 'axios';
+import StudentsCard from "./allStudentsCard"
 import '../styles/singleCampus.css'
 
 class SingleCampus extends Component {
   constructor() {
     super();
     this.state = {
-      campus:[]
+      campus: []
     }
   }
-    async componentDidMount () {
-      try{
-        let {data} = await axios.get(`https://campus-manager-api.herokuapp.com/campuses/${this.props.match.params.id}`)
-        this.setState({
-          campus:data[0]
-        })
-        console.log(data)
-      }
-      catch(err){
-        console.log(err);
-      }
+  async componentDidMount() {
+    this.fetchSingleCampus();
+  }
+
+  async fetchSingleCampus() {
+    try {
+      let { data } = await axios.get(`https://campus-manager-api.herokuapp.com/campuses/${this.props.match.params.id}/students`)
+      this.setState({
+        campus: data
+      })
+      console.log(data.students)
     }
+    catch(err) {
+      console.log(err);
+    }
+  }
+
+  // display = () =>(
+  //   this.state.campus.students.map(item=>{
+  //     return (
+  //       <StudentsCard
+  //       image={item.image}
+  //       firstName={item.firstname}
+  //       lastName={item.lastname}
+  //       removeStudent={this.props.removeStudent}
+  //       />
+  //     )
+  //   })
+  // )
+
   render() {
-    const {campus} = this.state;
+    const { campus } = this.state;
+
     return (
-        <div className="container">
-            <h1>{campus.name}</h1>
-            <div className="img">
-              <img src={campus.image}></img>
-            </div>
-            <div className="description">
-              <p>{campus.description}</p>
-            </div>
-            <div className="students">
-              <p>students</p>
-            </div>
+      <div className="container">
+        <h1>{campus.name}</h1>
+        <div className="img">
+          <img src={campus.image}></img>
         </div>
+        <div className="description">
+          <p>{campus.description}</p>
+        </div>
+        <div className="students">
+          {/* <p>{this.display()}</p> */}
+        </div>
+      </div>
     );
   }
 }
